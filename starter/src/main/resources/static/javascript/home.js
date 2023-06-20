4c2045db57b43e660e298612b74c4e7ad3e3e10f
Mitchell Nguyen <mitch01nguyen@gmail.com>
2023-06-20 13:55:19 -0500

    adds style and search

--- starter/src/main/resources/static/javascript/home.js
+++ starter/src/main/resources/static/javascript/home.js
@@ -7,11 +7,23 @@ const noteContainer = document.getElementById("note-container");
 let noteBody = document.getElementById("note-body");
 let updateNoteBtn = document.getElementById("update-note-button");
 
+const loginLink = document.getElementById("login-link")
+const registerLink = document.getElementById("register-link")
+
+const searchForm = document.getElementById("search-bar");
+const searchInput = document.getElementById("search-input");
+
+
 const headers = {
   "Content-type": "application/json",
 };
 
 const baseUrl = "http://localhost:8080/api/v1/notes";
+
+if(userId != undefined){
+  loginLink.innerHTML = "Logout";
+  registerLink.style.display = "None";
+}
 
 function handleLogout() {
   let c = document.cookie.split(";");
@@ -67,13 +79,18 @@ const handleNoteEdit = async (noteId) => {
     id: noteId,
     body: noteBody.value,
   };
-  await fetch(baseUrl+"/updateNote", {
+  await fetch(baseUrl + "/updateNote", {
     method: "PUT",
     body: JSON.stringify(body),
     headers: headers,
-  }).catch((err) => console.log(err.message));
-  return getNotes(userId);
+  })
+    .then(() => {
+      noteBody.innerHTML = "";
+      location.reload(); // Refresh the page
+    })
+    .catch((err) => console.log(err.message));
 };
+
 
 const handleDelete = async (noteId) => {
   await fetch(baseUrl +"/deleteNote/"+ noteId, {
@@ -85,13 +102,14 @@ const handleDelete = async (noteId) => {
 };
 
 const createNoteCards = (array) => {
-  console.log(array);
   noteContainer.innerHTML = "";
+  array.sort((a, b) => a.id - b.id)
   array.forEach((note) => {
     let noteCard = document.createElement("div");
     noteCard.classList.add("m-2");
     noteCard.innerHTML = `
         <div class="card d-flex " style="width: 18rem; height: 18rem;" >
+        <img src="img/pngwing.com (1).png" class="pin">
         <div class="card-body d-flex flex-column justify-content-between" style="height: available">
           <p class="card-text">${note.body}</p>
           <div class="d-flex justify-content-between">
@@ -99,24 +117,47 @@ const createNoteCards = (array) => {
             <button class="btn btn-primary" onclick="getNoteById(${note.id})" type="button" data-bs-toggle="modal" data-bs-target="#note-edit-modal-title">Edit</button>
           </div>
         </div>
-      </div> 
+      </div>
         `;
 
     noteContainer.appendChild(noteCard);
   });
 };
 
-const populateModal = obj => {
-    noteBody.innerText = ""
-    noteBody.innerText = obj.body
-    updateNoteBtn.setAttribute('data-note-id', obj.id)
-}
+const populateModal = (obj) => {
+  noteBody.innerText = obj.body;
+  updateNoteBtn.setAttribute("data-note-id", obj.id);
+};
+
+const getNotesSearched = async (userId) => {
+  let result = searchInput.value.toLowerCase();
+  await fetch(`${baseUrl}/notesByUser/${userId}`, {
+      method: "GET",
+      headers: headers,
+  })
+  .then((response) => response.json())
+  .then((data) => {
+      const filteredData = data.filter(note => {
+          let noteBody = note.body.toLowerCase();
+          return noteBody.includes(result);
+      });
+
+      createNoteCards(filteredData);
+  })
+  .catch((err) => console.log(err.message));
+};
+
 
 getNotes(userId)
 submitForm.addEventListener('submit', handleSubmit)
 
-updateNoteBtn.addEventListener('click',(e)=>{
-    let noteId = e.target.getAttribute('data-note-id')
-    handleNoteEdit(noteId)
-})
 
+updateNoteBtn.addEventListener('click', (e) => {
+  let noteId = e.target.getAttribute('data-note-id');
+  handleNoteEdit(noteId);
+});
+
+searchForm.addEventListener("submit", e => {
+  e.preventDefault();
+  getNotesSearched(userId);
+});
